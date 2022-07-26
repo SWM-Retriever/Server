9a8794738a73c9b18b313a2c630c9a9c36b3316a
bbeomgeun <miki308@naver.com>
2022-07-26 18:05:43 +0900

    PET-149 Init: 프로젝트 세팅

---
+++ src/main/java/org/retriever/server/dailypet/DailypetApplication.java
@@ -0,0 +1,13 @@
+package org.retriever.server.dailypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailypetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailypetApplication.class, args);
+	}
+
+}
