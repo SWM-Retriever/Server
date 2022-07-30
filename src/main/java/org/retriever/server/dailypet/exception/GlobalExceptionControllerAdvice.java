2acf271428c73c4b3b34f20468f14fde998d197b
Park Beom Geun (Eden) <miki30808@gmail.com>
2022-07-30 17:39:04 +0900

    PET-151 Feat: 로깅 프레임워크 세팅 (#6)
    * PET-151 Feat: 로깅 프레임워크 세팅
    
    * PET-151 Feat: 로깅 설정

--- src/main/java/org/retriever/server/dailypet/exception/GlobalExceptionControllerAdvice.java
+++ src/main/java/org/retriever/server/dailypet/exception/GlobalExceptionControllerAdvice.java
@@ -1,17 +1,20 @@
 package org.retriever.server.dailypet.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.retriever.server.dailypet.exception.dto.ApiErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionControllerAdvice {
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ApiErrorResponse> applicationException(ApplicationException exception) {
         String errorCode = exception.getErrorCode();
-
+        log.error("Exception : {} ErrorCode : {} Message : {}",
+                exception.getClass().getSimpleName(), errorCode,  exception.getMessage());
         return ResponseEntity
                 .status(exception.getHttpStatus())
                 .body(new ApiErrorResponse(errorCode));
