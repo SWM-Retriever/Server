613a19d0daafd802ea59763c49ffd50a74905c5f
bbeomgeun <miki308@naver.com>
2022-07-31 23:54:42 +0900

    PET-147 Feat: User 도메인 추가

---
+++ src/main/java/org/retriever/server/dailypet/user/RoleType.java
@@ -0,0 +1,10 @@
+package org.retriever.server.dailypet.user;
+
+import lombok.ToString;
+
+@ToString
+public enum RoleType {
+    ROLE_USER,
+    ROLE_MEMBERSHIP,
+    ROLE_ADMIN
+}
---
+++ src/main/java/org/retriever/server/dailypet/user/UserController.java
@@ -0,0 +1,15 @@
+package org.retriever.server.dailypet.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
