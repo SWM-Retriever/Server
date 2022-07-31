88eceb8e052ac1c111d13735b8ccbe043c76f13b
bbeomgeun <miki308@naver.com>
2022-07-31 23:57:06 +0900

    PET-147 Feat: jwt 기본 코드 추가

---
+++ src/main/java/org/retriever/server/dailypet/global/config/security/JwtTokenProvider.java
@@ -0,0 +1,87 @@
+package org.retriever.server.dailypet.global.config.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.crypto.SecretKey;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class JwtTokenProvider {
+
+    @Value("jwt.secret")
+    private String secretKey;
+
+    //@Value("jwt.expire")
+    private final Long expiredTime = 1000L * 60 * 60 * 24; // 1 day
+
+    private SecretKey key;
+
+    private final UserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+    public String createToken(String userId) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setSubject(userId)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setExpiration(new Date(now.getTime() + expiredTime))
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserId(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUserId(String token) {
+        return Jwts.parserBuilder().setSigningKey(secretKey)
+                .build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(jwtToken);
+            return claimsJws.getBody()
+                    .getExpiration()
+                    .after(new Date()); // 만료 기간이 현재 날짜보다 이후이면 아직 유효하다.
+        } catch (SecurityException e) {
+            log.info("Invalid JWT signature.");
+        } catch (MalformedJwtException e) {
+            log.info("Invalid JWT token.");
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT token.");
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT token.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT token compact of handler are invalid.");
+        }
+            return false;
+    }
+}
