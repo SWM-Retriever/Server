8b2575743399d246314b93364f3eaf4a68337340
bbeomgeun <miki308@naver.com>
2022-08-02 02:45:02 +0900

    PET-92 Feat: Jwt 인증, 인가 처리 클래스

--- src/main/java/org/retriever/server/dailypet/global/config/security/SecurityConfig.java
+++ src/main/java/org/retriever/server/dailypet/global/config/security/SecurityConfig.java
@@ -1,27 +1,30 @@
 package org.retriever.server.dailypet.global.config.security;
 
 import lombok.RequiredArgsConstructor;
+import org.retriever.server.dailypet.global.config.jwt.JwtAccessDeniedHandler;
+import org.retriever.server.dailypet.global.config.jwt.JwtAuthenticationEntryPoint;
 import org.retriever.server.dailypet.global.config.jwt.JwtAuthenticationFilter;
 import org.retriever.server.dailypet.global.config.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CorsFilter corsFilter;
     private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -31,20 +34,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                    .addFilter(corsFilter)
                     .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
 
                     .httpBasic().disable()
                     .csrf().disable()
                     .formLogin().disable()
                     .exceptionHandling()
-                    //.authenticationEntryPoint()
-                    //.accessDeniedHandler()
+                    .authenticationEntryPoint(jwtAuthenticationFilter)
+                    .accessDeniedHandler(jwtAccessDeniedHandler)
                 .and()
                     .authorizeRequests()
-                    .antMatchers(HttpMethod.POST, "").permitAll()
-                    .antMatchers(HttpMethod.GET, "").permitAll()
+                    .antMatchers(HttpMethod.POST, "/api/v1/test").permitAll()
+//                    .antMatchers(HttpMethod.GET, "").permitAll()
                     .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        , "/favicon.ico"
+                );
     }
 
     /*
