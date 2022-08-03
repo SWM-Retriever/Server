fc0c4e3f8c7c0080a535df037a92c92a97dfea82
bbeomgeun <miki308@naver.com>
2022-08-04 01:59:29 +0900

    Feat: BaseTimeEntity 적용

--- src/main/java/org/retriever/server/dailypet/domain/member/entity/Member.java
+++ src/main/java/org/retriever/server/dailypet/domain/member/entity/Member.java
@@ -2,12 +2,13 @@ package org.retriever.server.dailypet.domain.member.entity;
 
 import lombok.Getter;
 import org.retriever.server.dailypet.domain.member.enums.RoleType;
+import org.retriever.server.dailypet.domain.model.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
-public class Member {
+public class Member extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
