52a8c9e8b0f5068286936666d7f00e8afc76bc08
bbeomgeun <miki308@naver.com>
2022-08-05 00:05:29 +0900

    Feat: AOP를 이용한 request, response 로깅

--- src/main/java/org/retriever/server/dailypet/DailypetApplication.java
+++ src/main/java/org/retriever/server/dailypet/DailypetApplication.java
@@ -2,8 +2,10 @@ package org.retriever.server.dailypet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class DailypetApplication {
 
 	public static void main(String[] args) {
