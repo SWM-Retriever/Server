28816dc5c39a50c9b295255819c2b5e649f067c0
bbeomgeun <miki308@naver.com>
2022-08-05 20:32:28 +0900

    PET-105 Rename: KakaoOauthService.java -> MemberService.java

--- src/main/java/org/retriever/server/dailypet/domain/member/service/MemberService.java
+++ src/main/java/org/retriever/server/dailypet/domain/member/service/MemberService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class KakaoOauthService {
+public class MemberService {
 
     private final MemberRepository memberRepository;
 
