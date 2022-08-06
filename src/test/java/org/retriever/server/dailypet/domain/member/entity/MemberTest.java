cdafa899d68bcb06e6e4ad8f1707d314e02dc098
bbeomgeun <miki308@naver.com>
2022-08-07 02:41:14 +0900

    PET-211 Test: Member Entity unit test 작성

---
+++ src/test/java/org/retriever/server/dailypet/domain/member/entity/MemberTest.java
@@ -0,0 +1,41 @@
+package org.retriever.server.dailypet.domain.member.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.retriever.server.dailypet.domain.factory.MemberFactory;
+import org.retriever.server.dailypet.domain.member.dto.request.SignUpRequest;
+import org.retriever.server.dailypet.domain.member.enums.AccountStatus;
+import org.retriever.server.dailypet.domain.member.enums.RoleType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberTest {
+
+    @DisplayName("회원가입 request dto를 받아서 새로운 유저 생성")
+    @Test
+    void createNewMember() {
+
+        // given
+        SignUpRequest signUpRequest = MemberFactory.createSignUpRequest();
+
+        // when
+        Member newMember = Member.createNewMember(signUpRequest);
+
+        // then
+        assertThat(newMember.getId()).isNull();
+        assertThat(newMember.getNickName()).isEqualTo(signUpRequest.getSnsNickName());
+        assertThat(newMember.getEmail()).isEqualTo(signUpRequest.getEmail());
+        assertThat(newMember.getProfileImageUrl()).isEqualTo(signUpRequest.getProfileImageUrl());
+        assertThat(newMember.getProviderType()).isEqualTo(signUpRequest.getProviderType());
+        assertThat(newMember.getDeviceToken()).isEqualTo(signUpRequest.getDeviceToken());
+
+        assertThat(newMember.getAccountStatus()).isEqualTo(AccountStatus.ACTIVE);
+        assertThat(newMember.getRoleType()).isEqualTo(RoleType.MEMBER);
+        assertThat(newMember.getIsPersonalInformationAgree()).isTrue();
+        assertThat(newMember.getIsToSAgree()).isTrue();
+        assertThat(newMember.getIsPushAgree()).isFalse();
+
+        assertThat(newMember.getFamilyRoleName()).isEqualTo("별명을 입력해주세요!");
+        assertThat(newMember.getPassword()).isNull();
+    }
+}
