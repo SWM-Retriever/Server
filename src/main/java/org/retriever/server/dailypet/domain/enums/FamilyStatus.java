7c6ffd5cb45edd1e2add6a020a48d496961c0c64
bbeomgeun <miki308@naver.com>
2022-08-08 02:54:50 +0900

    PET-120 Feat: 가족 도메인 entity 추가

---
+++ src/main/java/org/retriever/server/dailypet/domain/enums/FamilyStatus.java
@@ -0,0 +1,6 @@
+package org.retriever.server.dailypet.domain.enums;
+
+public enum FamilyStatus {
+    ACTIVE,
+    DELETED;
+}
---
+++ src/main/java/org/retriever/server/dailypet/domain/family/dto/Family.java
@@ -0,0 +1,36 @@
+package org.retriever.server.dailypet.domain.family.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.retriever.server.dailypet.domain.enums.FamilyStatus;
+import org.retriever.server.dailypet.domain.model.BaseTimeEntity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Family extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long familyId;
+
+    @Enumerated(EnumType.STRING)
+    private FamilyStatus familyStatus;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String familyName;
+
+    private String invitationCode;
+
+    private String profileImageUrl;
+
+    @OneToMany(mappedBy = "family")
+    private List<FamilyMember> familyMemberList = new ArrayList<>();
+}
