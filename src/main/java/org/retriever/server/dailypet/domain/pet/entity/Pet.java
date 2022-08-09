29fcbc80939bd77ab36ca05c543d38310099db30
bbeomgeun <miki308@naver.com>
2022-08-09 17:26:04 +0900

    PET-214 Feat: 반려동물 도메인 entity 세팅 - Pet, PetKind

---
+++ src/main/java/org/retriever/server/dailypet/domain/pet/entity/Pet.java
@@ -0,0 +1,48 @@
+package org.retriever.server.dailypet.domain.pet.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.retriever.server.dailypet.domain.model.BaseTimeEntity;
+import org.retriever.server.dailypet.domain.pet.enums.Gender;
+import org.retriever.server.dailypet.domain.pet.enums.PetStatus;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Pet extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long petId;
+
+    @Column(nullable = false, length = 50)
+    private String petName;
+
+    private String profileImageUrl;
+
+    private LocalDateTime birthDate;
+
+    private Double weight;
+
+    @Column(unique = true, length = 50)
+    private String registerNumber;
+
+    @Column(nullable = false)
+    private Boolean isNeutered;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private PetStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "petKindId", nullable = false)
+    private PetKind petkind;
+}
