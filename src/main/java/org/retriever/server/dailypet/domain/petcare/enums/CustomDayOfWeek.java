db6e76cf0d60f30b65b9a68d3403df696bd88e46
bbeomgeun <miki308@naver.com>
2022-08-16 18:58:27 +0900

    Feat: PetCare enum 추가

---
+++ src/main/java/org/retriever/server/dailypet/domain/petcare/enums/CustomDayOfWeek.java
@@ -0,0 +1,11 @@
+package org.retriever.server.dailypet.domain.petcare.enums;
+
+public enum CustomDayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
