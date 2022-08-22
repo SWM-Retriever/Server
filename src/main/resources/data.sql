a8bbda97332f8f58e359e1ea619f5154c2de9b89
bbeomgeun <miki308@naver.com>
2022-08-22 17:43:56 +0900

    PET-215 Chore: 반려동물 품종 임시 데이터 insert data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (0 , now(), now(), '품종을 모르는 경우입니다.', 'Unknown', 'UNKNOWN');
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '리트리버', 'DOG');
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '포메라니안', 'DOG');
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '시베리안 허스키', 'DOG');
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '치와와', 'DOG');
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '푸들', 'DOG');
+
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '페르시안', 'CAT');
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '스코티시 폴드', 'CAT');
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '브리티시 쇼트헤어', 'CAT');
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '벵갈 고양이', 'CAT');
+INSERT INTO pet_kind (pet_kind_id, created_at, updated_at, information, pet_kind_name, pet_type) values (default , now(), now(), '', '메인쿤', 'CAT');
