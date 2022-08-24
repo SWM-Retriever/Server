a6ad0c46f928e0dfeee0a4f0ef8322fff7cd6306
bbeomgeun <miki308@naver.com>
2022-08-24 22:50:36 +0900

    Refactor: 이미지 없을 경우 null 처리

--- src/main/java/org/retriever/server/dailypet/global/utils/s3/S3FileUploader.java
+++ src/main/java/org/retriever/server/dailypet/global/utils/s3/S3FileUploader.java
@@ -25,6 +25,9 @@ public class S3FileUploader {
     private String bucket;
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
         File uploadFile = convert(multipartFile)
                 .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
 
