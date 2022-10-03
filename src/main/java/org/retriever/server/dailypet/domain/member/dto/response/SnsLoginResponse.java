b6c17d15544a5cc0b9742cb4fe518969d514c5ff
bbeomgeun <miki308@naver.com>
2022-10-03 16:53:41 +0900

    Feat: 로그인 응답에 familyId, petId 추가

--- src/main/java/org/retriever/server/dailypet/domain/member/dto/response/SnsLoginResponse.java
+++ src/main/java/org/retriever/server/dailypet/domain/member/dto/response/SnsLoginResponse.java
@@ -3,6 +3,9 @@ package org.retriever.server.dailypet.domain.member.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
@@ -17,4 +20,10 @@ public class SnsLoginResponse {
 
     @Schema(description = "jwt Token")
     private String jwtToken;
+
+    @Schema(description = "familyId")
+    private Long familyId;
+
+    @Schema(description = "petId List")
+    private List<Long> petIdList = new ArrayList<>();
 }
