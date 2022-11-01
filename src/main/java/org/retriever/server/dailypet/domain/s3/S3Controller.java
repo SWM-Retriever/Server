78309468df8dff6d5e2fdcd87a40a00da5ed6ccc
bbeomgeun <miki308@naver.com>
2022-11-02 01:19:04 +0900

    Remove: presignedurl jwt token 제거

--- src/main/java/org/retriever/server/dailypet/domain/s3/S3Controller.java
+++ src/main/java/org/retriever/server/dailypet/domain/s3/S3Controller.java
@@ -1,7 +1,6 @@
 package org.retriever.server.dailypet.domain.s3;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -23,14 +22,13 @@ public class S3Controller {
 
     private final S3FileUploader s3FileUploader;
 
-    @Parameter(name = "X-AUTH-TOKEN", description = "로그인 성공 후 access_token", required = true)
     @Operation(summary = "이미지 signedURL 반환 API", description = "파일의 이름 (dog.jpeg)과 종류에 맞는 enum type(MEMBER, PET, DIARY)을 넘겨서 signedURL을 얻는다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "정상 요청 완료"),
             @ApiResponse(responseCode = "400", description = "요청 실패"),
             @ApiResponse(responseCode = "500", description = "내부 서버 에러")
     })
-    @GetMapping("/PresignedUrl/{S3Path}/{fileName}")
+    @GetMapping("/presigned-url/{S3Path}/{fileName}")
     public ResponseEntity<PresignedUrlResponse> getPresignedUrl(@PathVariable S3Path S3Path, @PathVariable String fileName) {
         return ResponseEntity.ok(s3FileUploader.getPreSignedUrl(S3Path, fileName));
     }
